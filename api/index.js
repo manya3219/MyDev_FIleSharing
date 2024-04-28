f14ebecfb9c347351e0ceafe5cef34970a54baf3
manya3219 <yadavmanya1902@gmail.com>
2024-04-28 21:02:01 +0530

    add admin

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from'./routes/user.route.js';
-import authRouter from'./routes/auth.route.js';
+import userRoutes from'./routes/user.route.js';
+import authRoutes from'./routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 mongoose.connect(process.env.MONGO)
 .then(()=>{
@@ -20,15 +21,15 @@ app.listen(3000,()=>{
     console.log('Server is running on port 3000 ');
 });
 
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
 
-app.use((err,req,res,next)=>{
-    const statusCode=err.statusCode|| 500;
-    const message=err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message,
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
     });
 });
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,29 +1,34 @@
 import mongoose from 'mongoose';
 
-const userSchema =new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+      type: String,
+      required: true,
     },
-    profilePicture:{
-        type:String,
-        default:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw2aqRUS5IiKZcuCRPOqNdx8&ust=1714232482788000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNjs3uKb4IUDFQAAAAAdAAAAABAE',
+    profilePicture: {
+      type: String,
+      default:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
     },
-}, {timestamps:true}
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
 
-
-const User=mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
