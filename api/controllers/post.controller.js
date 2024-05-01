88decba1d7e50b84ee9175c251a57dfa2cd7ab6a
manya3219 <yadavmanya1902@gmail.com>
2024-05-01 20:42:47 +0530

    complete upload post image functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,17 +1,17 @@
 import{errorHandler}from'../utils/error.js'
 
 export const create=async(req,res,next)=>{
-    if(!req.user  .isAdmin){
-        returnnext(errorHandler(403,'you are not allowed to create a post'))
+    if(!req.user.isAdmin){
+        return next(errorHandler(403,'you are not allowed to create a post'))
 
     }
     if(!req.body.title|| req.body.content){
         return next(errorHandler(400,'please provide all required fields'))
 
     }
-    const slug=req.body.title.split(' ').join('-').toLowerCase(/[^a-zA-Z0-9]/G,'-');
+    const slug=req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g,'');
     const newPost=new Post({
-        ...req.body,slug,userId:req.user.id
+        ...req.body,slug,userId:req.user.id,
     });
     try{
         const savedPost=await newPost.save();
